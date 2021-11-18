7b1c8811acc3e4d04e8fdba297cace29a191ba5c
Karol <michkarol.2000@wp.pl>
2021-11-18 20:57:12 +0100

    Db context added to startup

--- OnlyFarms/Startup.cs
+++ OnlyFarms/Startup.cs
@@ -4,10 +4,12 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OnlyFarms.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace OnlyFarms
 {
@@ -23,6 +25,9 @@ namespace OnlyFarms
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<FarmContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaltConnection")));
+
             services.AddControllersWithViews();
         }
 
