de05b817890cf2d78bbe9443b8990ebe85661353
juliankrul <56558302+juliankrul@users.noreply.github.com>
2021-11-18 20:06:48 +0100

    Added Weather class

---
+++ OnlyFarms/Models/Weather.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlyFarms.Models
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+        public int Temperature { get; set; }
+        public int Moisture { get; set; }
+        public int AirPressure { get; set; }
+        public int RainfallAmount { get; set; }
+        public string WindDirection { get; set; }
+        public int WindSpeed { get; set; }
+        public DateTime Date { get; set; }
+        public Field Field { get; set; }
+    }
+}
