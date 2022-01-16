f952790a077dc164cbc245c2284b6a1ccf089588
Karol <michkarol.2000@wp.pl>
2022-01-16 19:55:20 +0100

    add Weather Service interface

---
+++ OnlyFarms/Services/IWeatherService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlyFarms.Services
+{
+    interface IWeatherService
+    {
+    }
+}
