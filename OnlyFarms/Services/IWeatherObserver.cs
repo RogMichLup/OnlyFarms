b10a54a0e18d7dd9785c4cfc8ab167c4bd0a2d4f
Karol <michkarol.2000@wp.pl>
2022-01-16 19:41:49 +0100

    add IWeatherObserver interface

---
+++ OnlyFarms/Services/IWeatherObserver.cs
@@ -0,0 +1,13 @@
+﻿using OnlyFarms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlyFarms.Services
+{
+    public interface IWeatherObserver
+    {
+        void Update(Weather weather);
+    }
+}
