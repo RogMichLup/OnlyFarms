bc1c6179f8ee3a91295cb4058040747a88b6f494
Karol <michkarol.2000@wp.pl>
2022-01-16 19:46:17 +0100

    add HarvestObserver

---
+++ OnlyFarms/Services/HarvestObserver.cs
@@ -0,0 +1,17 @@
+﻿using OnlyFarms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlyFarms.Services
+{
+    public class HarvestObserver : IWeatherObserver
+    {
+        public void Update(Weather weather)
+        {
+            //TODO: Some action with notifying of the harvest date change.
+            Console.WriteLine("Date of harvest is updated");
+        }
+    }
+}
