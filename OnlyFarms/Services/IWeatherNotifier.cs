d08f2ed2ee1cd8214624c7203f6d2686dc7f8dce
Karol <michkarol.2000@wp.pl>
2022-01-16 19:52:04 +0100

    add IWeatherNotifier interface

---
+++ OnlyFarms/Services/IWeatherNotifier.cs
@@ -0,0 +1,20 @@
+﻿using OnlyFarms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlyFarms.Services
+{
+    public interface IWeatherNotifier
+    {
+        // Attach a weather observer to the subject.
+        void Attach(IWeatherObserver observer);
+
+        // Detach a weather observer from the subject.
+        void Detach(IWeatherObserver observer);
+
+        // Notify all weather observers about an event.
+        void Notify(Weather weather);
+    }
+}
