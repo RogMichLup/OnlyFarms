d8108f314ff3fa7226ba8666ac33580ba60c704c
Karol <michkarol.2000@wp.pl>
2022-01-16 19:48:56 +0100

    add FertilizationObserver

---
+++ OnlyFarms/Services/FertilizationObserver.cs
@@ -0,0 +1,17 @@
+﻿using OnlyFarms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlyFarms.Services
+{
+    public class FertilizationObserver : IWeatherObserver
+    {
+        public void Update(Weather weather)
+        {
+            //TODO: Some action with notifying of the fertilization date change.
+            Console.WriteLine("Date of fertilization is updated");
+        }
+    }
+}
