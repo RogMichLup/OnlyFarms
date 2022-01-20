7205e4601d9721803b29d8896c1e32b5b8706041
juliankrul <56558302+juliankrul@users.noreply.github.com>
2022-01-20 18:26:48 +0100

    Add initial Memento pattern structure

---
+++ OnlyFarms/Memento/Caretaker.cs
@@ -0,0 +1,26 @@
+﻿using OnlyFarms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlyFarms.Memento
+{
+    public class Caretaker
+    {
+        private Snapshot backup;
+
+        public void MakeBackup(Procedure procedure)
+        {
+            backup = procedure.CreateSnapshot();
+        }
+
+        public void Undo()
+        {
+            if(backup != null)
+            {
+                backup.Restore();
+            }
+        }
+    }
+}
